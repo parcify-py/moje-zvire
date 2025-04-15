b8bbc8e610679d899e0c856fe1ea95bf329b1dcb
parcify <simonovmakar73@gmail.com>
2025-04-15 09:25:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 animal = input("Jake je tvoje oblibene zvire: ") 
 print(f"Tvoje oblibene zvire je {animal}")
+
+animal = input("Jake je tvoje oblibene zvire: ") 
+ print(f"Tvoje zvire je {animal}")
